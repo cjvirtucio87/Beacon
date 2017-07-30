106a254b859e77bde61ce3c178d30dc0e78d7847
cjvirtucio87 <cjv287@gmail.com>
2017-07-30 15:32:45 -0400

    Added target folder to gitignore.

--- target/classes/com/cjvirtucio/beacon/sql/district.seed.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO district(DistrictName) VALUES ('CENTRAL');
-INSERT INTO district(DistrictName) VALUES ('EASTERN');
-INSERT INTO district(DistrictName) VALUES ('NORTHEASTERN');
-INSERT INTO district(DistrictName) VALUES ('NORTHERN');
-INSERT INTO district(DistrictName) VALUES ('NORTHWESTERN');
-INSERT INTO district(DistrictName) VALUES ('SOUTHEASTERN');
-INSERT INTO district(DistrictName) VALUES ('SOUTHERN');
-INSERT INTO district(DistrictName) VALUES ('SOUTHWESTERN');
-INSERT INTO district(DistrictName) VALUES ('WESTERN');
--- target/classes/com/cjvirtucio/beacon/sql/district.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `district` (
-   `Id` INT NOT NULL AUTO_INCREMENT,
-   `DistrictName` VARCHAR(12),
-   PRIMARY KEY (`Id`)
-)$$
